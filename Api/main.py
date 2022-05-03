0c693b04226ac62daa3c523793d240ff1b8a138d
Mireu / 미르 <limmireu@kakao.com>
2022-05-03 09:25:01 +0000

    upload

--- Api/main.py
+++ Api/main.py
@@ -1,1 +1,14 @@
-from fastapi import Fastapi
+# SQL 모듈 불러오기
+from .carnation import read, write
+
+from fastapi import FastAPI, UploadFile, File
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def main():
+    return {"Docs" : "http://exec.mireu.xyz/"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=80)
