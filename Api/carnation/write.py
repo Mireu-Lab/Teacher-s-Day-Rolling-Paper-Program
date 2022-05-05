9dd0fb4d17b84dfb94079f91eb285cceed8fcc6d
limmireu <limmireu@kakao.com>
2022-05-05 13:33:13 +0900

    코드 수정 및 추가 API 기본 세팅

--- Api/carnation/write.py
+++ Api/carnation/write.py
@@ -2,15 +2,10 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-import json
+import re
 
-with open("/Date/info.json", "r") as program_info:
-    project_info = program_info.read()
-
-cred = credentials.Certificate(project_info["Firebase"]["Key"])
-firebase_admin.initialize_app(cred,{
-    'databaseURL' : project_info["Firebase"]["URL"]
-})
+cred = credentials.Certificate("Data/carnation-mireu-xyz-firebase-adminsdk-p3fgl-a1df3d92e0.json")
+firebase_admin.initialize_app(cred)
 
 school = None
 teacher_name = None
--- Api/main.py
+++ Api/main.py
@@ -1,14 +1,29 @@
 # SQL 모듈 불러오기
 from carnation import read, write
 
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI
+from pydantic import BaseModel
 import uvicorn
 
 app = FastAPI()
 
+class SetData(BaseModel):
+    school = str
+    teacher_name = str
+    class_number = int
+    grade = int
+
 @app.get("/")
-def main():
+async def main():
     return {"Docs" : "http://execpro.mireu.xyz/"}
+
+@app.post("/write")
+async def write():
+    pass
+
+@app.post("/read")
+async def read():
+    pass
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=80)
