809eb829f94b9acba3fd5773eb062bbe61366792
limmireu <limmireu@kakao.com>
2022-05-05 15:09:50 +0900

    API 세팅

--- Api/main.py
+++ Api/main.py
@@ -7,23 +7,41 @@ import uvicorn
 
 app = FastAPI()
 
-class SetData(BaseModel):
+class ReadData(BaseModel):
     school = str
     teacher_name = str
     class_number = int
     grade = int
+
+class WriteData(BaseModel):
+    school = str
+    teacher_name = str
+    grade = int
+    class_number = int
+    number = int
+    name = str
 
 @app.get("/")
 async def main():
     return {"Docs" : "http://execpro.mireu.xyz/"}
 
 @app.post("/write")
-async def write():
-    pass
+async def write_api(WriteData:WriteData, mamo:str):
+    write.school = WriteData.school
+    write.teacher_name = WriteData.teacher_name
+    write.grade = WriteData.grade
+    write.class_number = WriteData.class_number
+    write.number = WriteData.number
+    write.name = WriteData.name
+    return write.write(mamo)
 
 @app.post("/read")
-async def read():
-    pass
+async def read_api(ReadData:ReadData):
+    read.school = ReadData.school
+    read.teacher_name = ReadData.teacher_name
+    read.class_number = ReadData.class_number
+    read.grade = ReadData.grade
+    return read.read()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=80)
